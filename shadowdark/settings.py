f52d4c2ec2da2a9d43f25850f2f328695109e1cc
Imad Ottallah <imadotta@gmail.com>
2023-04-06 20:49:57 -0500

    setup

--- shadowdark/settings.py
+++ shadowdark/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jf3pj_fvmw$xiixn+%0*%rlee^or)u$lr3zl-vt3jxk!_ez$&8'
+# SECRET_KEY = 'django-insecure-jf3pj_fvmw$xiixn+%0*%rlee^or)u$lr3zl-vt3jxk!_ez$&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
